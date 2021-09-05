7822d3b7d1952500b6a09b3aea93cc9e4029b1bf
Michal <michalrosa17@gmail.com>
2021-09-05 18:09:47 +0200

    integrate eslint with prettier

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'airbnb-typescript'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
