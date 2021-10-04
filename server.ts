385bd016a0d3ac3512ad7411b452ab940f8f533e
Michal <michalrosa17@gmail.com>
2021-10-04 18:14:06 +0200

    remove Procfile

--- server.ts
+++ server.ts
@@ -34,4 +34,4 @@ if (port) {
   });
 }
 
-app.listen(port, () => console.log('Server is running'));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
--- ts-build/server.js
+++ ts-build/server.js
@@ -27,4 +27,4 @@ if (port) {
         res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
     });
 }
-app.listen(port, function () { return console.log('Server is running'); });
+app.listen(port, function () { return console.log("Server is running on port " + port); });
