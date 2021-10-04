ed5451ec3006da6493d0005a86ad7b7b81e7ccbc
Michal <michalrosa17@gmail.com>
2021-10-04 19:36:15 +0200

    recompile build

--- ts-build/utils/songsterQueryParameters.js
+++ ts-build/utils/songsterQueryParameters.js
@@ -4,7 +4,7 @@ var params = {
     client_id: '378f1ce509b643ae809011e62f85b8d9',
     response_type: 'code',
     redirect_uri: process.env.NODE_ENV === 'production'
-        ? ''
+        ? 'https://fast-ridge-27303.herokuapp.com/redirect'
         : 'http://localhost:5000/redirect',
     state: '34fFs29kd09',
     scope: 'user-read-recently-played user-top-read user-library-read playlist-read-private',
